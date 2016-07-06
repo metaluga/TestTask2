35f45e64551fda931f930f42a24bf3a581cda62a
Владислав <metaluga1995@gmail.com>
2016-07-07 01:23:38 +0300

    Final with test

---
+++ TestTask2/Image.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTask2
+{
+    class Image
+    {
+
+
+
+
+        public static int[,] moves_important_images(int[,] img, int pic_width, int pic_height, int pic_x, int pic_y)
+        {
+            if ((pic_x == 0) || (pic_y == 0))
+                return img;
+
+            for (int i = 0; i < pic_height; i++)
+                for (int j = 0; j < pic_width; j++)
+                    img[i, j] = img[i + pic_y, j + pic_x];
+
+            return img;
+        }
+
+
+    }
+}
--- TestTask2/Program.cs
+++ TestTask2/Program.cs
@@ -10,6 +10,39 @@ namespace TestTask2
     {
         static void Main(string[] args)
         {
+            int frame_width = 60;
+            int frame_height = 80;
+
+            int[,] myImg = new int[frame_height, frame_width];
+
+            int k = 1;
+
+            for (int i =0; i < frame_height; i++)
+                for (int j = 0; j < frame_width; j++)
+                {
+                    if (((i >= 10) && (i <= 40)) && ((j >= 15) && (j <= 45)))
+                        myImg[i, j] = k;
+                    else
+                        myImg[i, j] = 0;
+                }
+
+            for (int i = 0; i < frame_height; i++)
+            {
+                for (int j = 0; j < frame_width; j++)
+                    Console.Write(myImg[i, j]);
+                Console.WriteLine();
+            }
+            Console.WriteLine("\n");
+
+
+            Image.moves_important_images(myImg, 31, 31, 15, 10);
+
+            for (int i = 0; i < frame_height; i++)
+            {
+                for (int j = 0; j < frame_width; j++)
+                    Console.Write(myImg[i, j]);
+                Console.WriteLine();
+            }
         }
     }
 }
